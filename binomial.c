9cd6b642f85cd56e821de185806e8e23861951cc
Matthew Rinne <mdrinne@att.net>
2018-04-23 18:44:44 -0500

    added binomial files

---
+++ binomial.c
@@ -0,0 +1,106 @@
+//  CS201 Assign3 binomial.c
+//  Matthew Rinne
+// This file implements a Binomial Heap
+// April 2018
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "bst.h"
+#include "queue.h"
+#include "avl.h"
+#include "binomial.h"
+
+
+struct binomial
+{
+
+}
+
+
+extern BINOMIAL *
+newBINOMIAL(
+    void (*display)(void *,FILE *),
+    int (*compare)(void *,void *),
+    void (*update)(void *,void *),
+    void (*free)(void *))
+{
+
+}
+
+
+extern void *
+insertBINOMIAL(BINOMIAL *b,void *value)
+{
+
+}
+
+
+extern int
+sizeBINOMIAL(BINOMIAL *b)
+{
+
+}
+
+
+extern void
+unionBINOMIAL(BINOMIAL *,BINOMIAL *)
+{
+
+}
+
+
+extern void
+deleteBINOMIAL(BINOMIAL *b,void *node)
+{
+
+}
+
+
+extern void
+decreaseKeyBINOMIAL(BINOMIAL *b,void *node,void *value)
+{
+
+}
+
+
+extern void *
+peekBINOMIAL(BINOMIAL *b)
+{
+
+}
+
+
+extern void *
+extractBINOMIAL(BINOMIAL *b)
+{
+
+}
+
+
+extern void
+statisticsBINOMIAL(BINOMIAL *b,FILE *fp)
+{
+
+}
+
+
+extern void
+displayBINOMIAL(BINOMIAL *b,FILE *fp)
+{
+
+}
+
+
+extern void
+displayBINOMIALdebug(BINOMIAL *b,FILE *fp)
+{
+
+}
+
+
+extern void
+freeBINOMIAL(BINOMIAL *b)
+{
+  
+}
